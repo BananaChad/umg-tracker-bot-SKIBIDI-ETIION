59ea6f933852497b23ff566fca4b6f70dac057e3
syaro <70029137+ffxfusion@users.noreply.github.com>
2024-01-30 11:54:13 +0000

    Update dumper.js
    made it support variant tracker channels in the discord
    
    also made it more optimized.

--- dumpbot/dumper.js
+++ dumpbot/dumper.js
@@ -7,61 +7,60 @@ client.on("ready", async () => {
 });
 
 let embedData = {
-  ore: "",
-  variant: "", //no variant detection here yet
-  miner: "",
-  position: "",
-  pickaxe: "",
-  rawChance: "",
-  rng: "",
   rare: false,
 };
 
-const hook = new WebhookClient({
-  url: "REDACTED",
-});
-
 client.on("messageCreate", async (message) => {
-  if (
-    message.guild.id === "1059506407826788393" &&
-    message.channel.id === "1059846328772993064" &&
-    message.author.id === "1088664915457347594"
-  ) {
-    const data = message.embeds[0].description
-      .split(" | ")
-      .map((item) => item.split("**")[1]);
-
-    [
-      embedData.miner,
-      embedData.position,
-      embedData.pickaxe,
-      embedData.rawChance,
-    ] = data;
-
-    embedData.ore = message.embeds[0].title;
-    embedData.rng = data[3].split("**")[3];
-
-    if (message.content) {
-      embedData.rare = true;
-    }
-
-    //sends to proxy server
-    hook.send({
+  const guildId = "1059506407826788393";
+  const trackers = {
+    "1059846328772993064": {
+      authorId: "1088664915457347594",
+      hook: hooks.rarespawnHook,
       content: embedData.rare ? "ping" : " ",
-      embeds: [message.embeds[0]],
-    });
+    },
+    "1059846357860491376": {
+      authorId: "1088664394130534421",
+      hook: hooks.normalTrackerHook,
+    },
+    "1173767047080054784": {
+      authorId: "1173767219549843527",
+      hook: hooks.alteredTrackerHook,
+    },
+    "1173773186240888842": {
+      authorId: "1173773260542988483",
+      hook: hooks.superAlteredTrackerHook,
+    },
+    "1173778041600757760": {
+      authorId: "1173778285767950436",
+      hook: hooks.giganticTrackerHook,
+    },
+    "1173778098379034705": {
+      authorId: "1173779194933682197",
+      hook: hooks.tinyTrackerHook,
+    },
+    "1173778128921960518": {
+      authorId: "1173779668030206032",
+      hook: hooks.evilTrackerHook,
+    },
+  };
+
+  if (message.guild.id === guildId) {
+    const tracker = trackers[message.channel.id];
+    if (tracker && message.author.id === tracker.authorId) {
+      if (message.content && tracker === trackers["1059846328772993064"]) {
+        embedData.rare = true;
+      }
+
+      tracker.hook.send({
+        content: tracker.content || " ",
+        embeds: [message.embeds[0]],
+      });
+    }
   }
 
   embedData = {
-    ore: "",
-    variant: "",
-    miner: "",
-    position: "",
-    pickaxe: "",
-    rawChance: "",
-    rng: "",
     rare: false,
   };
 });
 
-client.login("REDACTED");
+client.login("token here");
